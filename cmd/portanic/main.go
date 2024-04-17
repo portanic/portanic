10be3c5c9eb0293c600601d3df884e4bbad5528a
MariusKimmina <mar.kimmina@gmail.com>
2024-04-17 20:29:20 +0200

    add coreui admin template

--- cmd/portanic/main.go
+++ cmd/portanic/main.go
@@ -7,10 +7,13 @@ import (
 
 func main() {
     http.HandleFunc("/", serveHome)
+    http.Handle("/css/", http.StripPrefix("/css/", http.FileServer(http.Dir("static/css/"))))
+    http.Handle("/js/", http.StripPrefix("/js/", http.FileServer(http.Dir("static/js/"))))
+    http.Handle("/vendors/", http.StripPrefix("/vendors/", http.FileServer(http.Dir("static/vendors/"))))
     fmt.Println("Server is running on http://localhost:8080")
     http.ListenAndServe(":8080", nil)
 }
 
 func serveHome(w http.ResponseWriter, r *http.Request) {
-    http.ServeFile(w, r, "templates/index.html")
+    http.ServeFile(w, r, "static/index.html")
 }
